26c5cfbad97d31ff5700a436d09092cbbbd490a5
IvanKapytsia <ivan628126@gmail.com>
2025-03-21 01:12:20 +0100

    task-3

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import userData from "./userData.json";
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./friends.json";
 
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./transactions.json";
+
 const App = () => {
   return (
     <>
@@ -17,6 +20,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
